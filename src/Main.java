fa0771cf0e3bd97325bc5a3d47eb92eaf91327ef
Fromg <nittyfrong@gmail.com>
2019-07-01 13:01:58 +0700

    Frong

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+
+public class Main {
+
+	public static void main(String args[]) {
+//		int a = 10 ;
+//		double price = 10.5 ;
+//		String name = "Frong" ;
+//		
+//		for(int i=0; i<5; i++)
+//		System.out.println(name);
+//		
+//		int num[] = {1,2,3,4,9};
+//		for(int i=0; i<num.length; i++)
+//			System.out.println(num[i]);
+//		
+//		int[] num2 = new int[100] ;
+//		
+//		
+//		String[] fruits = {"banana","apple","апеп║м"};
+//		for(String f : fruits)
+//			System.out.println(f) ;
+		
+		String tel = "053-211-222" ;
+		String[] ts = tel.split("-");
+	    for(String t : ts)
+	    	System.out.println(t);
+		
+			
+		
+		
+	
+	
+}
+}
